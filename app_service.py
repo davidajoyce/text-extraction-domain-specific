f32e6347f4399b473a893ae48d435a6a788fb223
David Joyce <davidajoyce141@gmail.com>
2022-06-06 18:33:59 +1000

    feat: add search to api

--- app_service.py
+++ app_service.py
@@ -1,4 +1,35 @@
 import json
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents.indexes import SearchIndexClient 
+from azure.search.documents import SearchClient
+from azure.search.documents.indexes.models import (
+    ComplexField,
+    CorsOptions,
+    SearchIndex,
+    ScoringProfile,
+    SearchFieldDataType,
+    SimpleField,
+    SearchableField
+)
+from entityExtraction import findFinanceTerms
+from searchcognitiveservice import findDescriptions
+
+service_name = "financetermdetails"
+admin_key = "x6u7a1gn9oajlKtFIA6GNg7gCgdmx13V0Ndjkmnay2AzSeBtkkZO"
+
+index_name = "finance-term-full"
+
+# Create an SDK client
+endpoint = "https://financetermdetails.search.windows.net/".format(service_name)
+admin_client = SearchIndexClient(endpoint=endpoint,
+                    index_name=index_name,
+                    credential=AzureKeyCredential(admin_key))
+
+search_client = SearchClient(endpoint=endpoint,
+                       index_name=index_name,
+                       credential=AzureKeyCredential(admin_key))
+
 
 class AppService:
     
@@ -21,6 +52,10 @@ class AppService:
     ]
 
     def __init__(self):
+        #results =  search_client.search(search_text="wifi", include_total_count=True, select='FinanceTermId,FinanceTerm,Description,Url')
+        #print ('Total Documents Matching Query:', results.get_count())
+        #for result in results:
+        #    print("{}: {}: {}".format(result["FinanceTermId"], result["FinanceTerm"], result["Description"], result["Url"]))
         self.tasksJSON = json.dumps(self.tasks)
 
     def get_tasks(self):
@@ -47,3 +82,13 @@ class AppService:
                 tasksData.remove(task)
                 return json.dumps(tasksData);
         return json.dumps({'message': 'task id not found'});
+
+    def extract_terms(self, data):
+        financeTerms = findFinanceTerms(data)
+        print("financeTerms")
+        print(financeTerms)
+        financeTermToDescription = findDescriptions(financeTerms)
+        financeTermsJson = json.dumps(financeTerms)
+        return financeTermsJson
+
+
--- searchcognitiveservice.py
+++ searchcognitiveservice.py
@@ -12,8 +12,8 @@ from azure.search.documents.indexes.models import (
     SearchableField
 )
 
-service_name = ""
-admin_key = ""
+service_name = "financetermdetails"
+admin_key = "x6u7a1gn9oajlKtFIA6GNg7gCgdmx13V0Ndjkmnay2AzSeBtkkZO"
 
 index_name = "finance-term-full"
 
@@ -43,6 +43,28 @@ def upload_files(document_to_upload):
         print("Upload of new document succeeded: {}".format(result[0].succeeded))
     except Exception as ex:
         print (ex.message)
+
+def findDescriptions(financeTerms):
+    details = []
+    print("trying to find the descriptions")
+    for term in financeTerms:
+        termSplit = term.split(" ")
+        searchString = ""
+        for word in termSplit:
+            searchString = searchString + word
+            searchString = searchString + "~" + " "
+        print("searchString")
+        print(searchString)
+
+        results = search_client.search(search_text=searchString, query_type="full", top=1, include_total_count=True, select='FinanceTermId,FinanceTerm,Description,Url')
+        #print ('Total Documents Matching Query:', result.get_count())
+        print(results)
+        print ('Total Documents Matching Query:', results.get_count())
+        #print("{}: {}: {} : {}".format(result["FinanceTermId"], result["FinanceTerm"], result["Description"], result["Url"], result["@search.score"]))
+        for result in results:
+            print("{}: {}: {} : {}".format(result["FinanceTermId"], result["FinanceTerm"], result["Description"], result["Url"], result["@search.score"]))
+            print(result)
+    return details
 
 
 if __name__ == "__main__":
